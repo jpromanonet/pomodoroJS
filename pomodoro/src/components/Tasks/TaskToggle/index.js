f93a875832a53781816c84597ca2d5f8a2a921e3
jpromanonet <programming@jpromano.net>
2021-10-03 21:15:42 -0300

    Task - TasksList style

--- pomodoro/src/components/Tasks/TaskToggle/index.js
+++ pomodoro/src/components/Tasks/TaskToggle/index.js
@@ -3,10 +3,12 @@ import '.styles.css';
 
 const TaskToggle = ({ task, toggleTask}) => (
     <button
-        className={`ToggleTask ${task &&  'active'}`}
+        className={`ToggleTask ${task && 'active'}`}
         onClick={toggleTask}
         title={task ? 'Disable Task' : 'Enable Task'}
     >
         <i className={'fa fa-tasks'} />
     </button>
-)
+);
+
+export default memo(TaskToggle);
